ba90a093ebe3a2e3971c6d92bb208b0ecfb713e3
idrazovic <idrazovic@gmail.com>
2024-07-07 23:33:39 +0200

    added file storage

--- backend/src/controllers/messages.ts
+++ backend/src/controllers/messages.ts
@@ -1,28 +1,56 @@
+import path from "path";
+import fs from "fs";
+
 import { NextFunction, Request, Response } from "express";
+
 
 import { Message } from "../models/message";
 
-
-const getMessages = async (req: Request, res: Response, next: NextFunction) => {
+const getMessages = (req: Request, res: Response, next: NextFunction) => {
     try {
-        const messages: Message[] = [
-            {
-                id: '1',
-                text: 'Hello, World!',
-                user: 'admin',
-                timestamp: new Date().toISOString()
-            },
-            {
-                id: '2',
-                text: 'Hello, World 2!',
-                user: 'admin',
-                timestamp: new Date().toISOString()
+        fs.readFile(path.join(__dirname, '..', 'data', 'data.json'), 'utf-8', (err, data) => {
+            if (err) {
+                throw err;
             }
-        ]
-        res.status(200).json({ messages: messages });
+
+            if (!data) {
+                res.status(200).json({ messages: [] });
+                return;
+            }
+
+            res.status(200).json({ messages: JSON.parse(data) });
+        })
     } catch (error) {
         throw error;
     }
 };
 
-export { getMessages };
+const postMessage = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const message: Message = {
+            id: new Date().toISOString(),
+            text: req.body.text,
+            user: 'admin',
+            timestamp: new Date().toISOString()
+        }
+        fs.readFile(path.join(__dirname, '..', 'data', 'data.json'), 'utf-8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            const messages = JSON.parse(data ? data : '[]');
+            messages.push(message);
+            fs.writeFile(path.join(__dirname, '..', 'data', 'data.json'), JSON.stringify(messages), err => {
+                if (err) {
+                    throw err;
+                }
+
+                res.status(201).json({ message: message });
+            })
+        })
+    } catch (error) {
+        throw error;
+    }
+}
+
+export { getMessages, postMessage };
